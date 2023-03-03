dd3f98dc6edfc98841da6ce94cd68135b4beecf5
StevensMidnightBlessings <115166209+StevensMidnightBlessings@users.noreply.github.com>
2023-03-03 16:14:21 -0500

    Create RemoveLastCommand.java

---
+++ recitation7/RemoveLastCommand.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class RemoveLastCommand extends UndoCommand {
+	private ArrayList<String> sourceList;
+	
+	public RemoveLastCommand(ArrayList<String> lst) {
+		this.sourceList = lst;
+	}
+	
+	@Override
+	public void execute() {
+		int index = sourceList.size() - 1;
+		sourceList.remove(index);
+	}
+}
